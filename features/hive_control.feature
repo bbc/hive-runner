41ed2672730217a7f1309365aa6901202f8a7d1e
Joe Haig <joe.haig@bbc.co.uk>
2015-01-10 16:55:33 +0000

    Clean up cucumber tests

--- features/hive_control.feature
+++ features/hive_control.feature
@@ -12,25 +12,19 @@ Feature: Hive control
     Then the runner is not running
     
   Scenario: Starting a shell hive
-    Given hive is configured to use a shell worker
+    Given hive is configured to use 1 shell worker(s)
     When I start the runner
     Then the runner loads the shell controller
     And 1 shell worker(s) are running
     
   Scenario: Starting a hive with multiple shell workers
-    Given hive is configured to use 2 shell workers
+    Given hive is configured to use 2 shell worker(s)
     When I start the runner
     Then the runner loads the shell controller
     And 2 shell worker(s) are running
 
-  #Scenario: Stopping a hive (old)
-  #  Given a hive is running with a shell worker
-  #  When I stop the runner
-  #  Then the runner process terminates
-  #  And the shell worker terminates
-
   Scenario: Stopping a hive
-    Given hive is configured to use a shell worker
+    Given hive is configured to use 1 shell worker(s)
     And The runner has been started
     When I stop the runner
     Then the runner is not running
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -5,7 +5,6 @@ end
 
 After do
   `bundle exec ./bin/hived stop`
-  #`killall WORKER`
   sleep 1
 end
 
@@ -25,17 +24,17 @@ When(/^I stop the runner$/) do
 end
 
 Then(/^the runner is running$/) do
+  # TODO: Does this need two checks?
   expect(`bundle exec ./bin/hived status`).to match(/: running \[pid/)
+  expect(`ps aux | grep HIVE | grep -v grep | wc -l`.to_i).to be 1
 end
 
 Then(/^the runner is not running$/) do
+  # TODO: Does this need two checks?
   # TODO: Capture the stderr
   # expect(`bundle exec ./bin/hived status`).to match(/: no instances running/)
   expect(`bundle exec ./bin/hived status`).to match(/^$/)
-end
-
-Given(/^hive is configured to use a shell worker$/) do
-  ENV['HIVE_CONFIG'] = File.expand_path('../../configurations/one_shell_worker.yml', __FILE__)
+  expect(`ps aux | grep HIVE | grep -v grep | wc -l`.to_i).to be 0
 end
 
 Then(/^the runner loads the shell controller$/) do
@@ -46,24 +45,12 @@ Then(/^the shell worker is running$/) do
   expect(`bundle exec ./bin/hived status`).to match(/- shell worker \[pid/)
 end
 
-Given(/^hive is configured to use (\d+) shell workers$/) do |arg1|
-  ENV['HIVE_CONFIG'] = File.expand_path("../../configurations/#{arg1}_shell_workers.yml", __FILE__)
+Given(/^hive is configured to use (\d+) shell worker\(s\)$/) do |arg1|
+  ENV['HIVE_CONFIG'] = create_configuration(n_workers: arg1.to_i)
 end
 
 Then(/^(\d+) shell worker\(s\) are running$/) do |arg1|
   expect(`ps aux | grep SHELL_WORKER | grep -v grep | wc -l`.to_i).to be arg1.to_i
-end
-
-Given(/^a hive is running with a shell worker$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the runner process terminates$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^the shell worker terminates$/) do
-  pending # express the regexp above with the code you wish you had
 end
 
 Given(/^the shell worker is configured to use queues 'shell_queue_one' and 'shell_queue_two'$/) do
@@ -76,4 +63,21 @@ end
 
 Then(/^the shell worker polls queue 'shell_queue_one' and 'shell_queue_two'$/) do
   pending # express the regexp above with the code you wish you had
+end
+
+# TODO: Do this a bit better, and perhaps put in a 'helpers' file
+def create_configuration(options = {})
+  name = File.expand_path('../../tmp/config.yml', __FILE__)
+  File.open(name, 'w') do |f|
+    f.puts '---'
+    f.puts 'controllers:'
+    f.puts '  shell:'
+    f.puts "    max_workers: #{options[:n_workers] || 5}"
+    f.puts '    name_stub: SHELL_WORKER'
+    f.puts 'logging:'
+    f.puts '  directory: features/tmp'
+    f.puts '  pids: features/tmp'
+    f.puts '  main_filename: hive.log'
+  end
+  name
 end
