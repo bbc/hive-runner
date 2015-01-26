48050855ba10fe9a42be162b3dcf1ce214f9e6ad
Joe Haig <joe.haig@bbc.co.uk>
2015-01-26 12:34:03 +0000

    Add execution variables to execution scripts

--- lib/hive/execution_script.rb
+++ lib/hive/execution_script.rb
@@ -11,12 +11,36 @@ module Hive
         'HIVE_WORKING_DIRECTORY' => job_paths.testbed_path,
         'RESULTS_FILE' => job_paths.results_file
       }
+      @env_unset = [
+        'BUNDLE_GEMFILE',
+        'BUNDLE_BIN_PATH',
+        'GEM_PATH',
+        'RUBYOPT',
+        'rvm_'
+      ]
       @script_lines = []
     end
 
     def append_bash_cmd(shell_command)
       @log.debug "bash.rb - Appending bash command to #{@path} script: " + shell_command
       @script_lines << shell_command
+    end
+
+    def set_env(var, value)
+      @env[var] = value
+
+      # TODO What if the element appears multiple times?
+      if (i = @env_unset.index(var))
+        @env_unset.delete(i)
+      end
+
+      ## In Ruby 2, replace the above 'if' block with ...
+      #@env_unset.remove(var)
+    end
+
+    def unset_env(var)
+      @env.delete(var)
+      @env_unset << var
     end
 
     def run
@@ -26,6 +50,9 @@ module Hive
         f.write("# Set environment\n")
         @env.each do |key, value|
           f.write("export #{key}=#{value}\n")
+        end
+        @env_unset.each do |var|
+          f.write("unset #{var}\n")
         end
         f.write("cd $HIVE_WORKING_DIRECTORY")
         f.write("\n# Test execution\n")
