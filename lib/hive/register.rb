cbbcfdafdf3f71c85706121107216f6fc2cf8f2d
asim <asimk21@gmail.com>
2015-06-29 10:24:34 +0100

    removed pry calls

--- lib/hive/register.rb
+++ lib/hive/register.rb
@@ -28,8 +28,6 @@ module Hive
         Hive.logger.info("Adding controller for '#{type}'")
         require "hive/controller/#{type}"
         controller = Object.const_get('Hive').const_get('Controller').const_get(type.capitalize).new(opts.to_hash)
-        # require 'pry'
-        # binding.pry
         @controllers << controller
       end
       check_controllers
@@ -75,6 +73,7 @@ module Hive
           (new_device_list[c.class] - @devices[c.class]).each do |d|
             @devices[c.class] << d
           end
+  
           # Check that all known devices have running workers
           @devices[c.class].each do |d|
             if d.claimed?
