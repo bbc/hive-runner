78633aebf5ba5fc9bb52609eced060a7fe0a18ea
asim <asimk21@gmail.com>
2015-06-30 10:23:41 +0100

    Added temporary should_run?

--- lib/hive/diagnostic_runner.rb
+++ lib/hive/diagnostic_runner.rb
@@ -5,9 +5,9 @@ module Hive
 			@diagnostic = []
 			@options = options
 			@diagnostic_type = diagnostic_type
-			@diagnostic_array = Hive.config.diagnostic[@diagnostic_type].keys #read_config() # Read,  and store in array
-        	initialize_diagnostic(Hive.config.diagnostic) # Use array to initialize objects + capitalize
-	    	run_diagnostic unless !should_run?# Run method in subclasses calling base class method 	
+			@diagnostic_array = Hive.config.diagnostic[@diagnostic_type].keys 
+        	initialize_diagnostic(Hive.config.diagnostic) 
+        	run_diagnostic unless !should_run? 	
     	end
 
     	def initialize_diagnostic(diagnostic_details)
@@ -15,20 +15,25 @@ module Hive
       			Hive.logger.info("Initializing #{component.capitalize} component for #{@diagnostic_type.capitalize} diagnostic")
         		require "hive/diagnostic/#{@diagnostic_type}/#{component}"
         		@diagnostic << Object.const_get('Hive').const_get('Diagnostic').const_get(@diagnostic_type.capitalize).const_get(component.capitalize).new(diagnostic_details[@diagnostic_type][component])
-        		#@diagnostic << diagnostic
-      		}
+        	}
     	end 
 
     	def should_run?
+    		 # 	previous_timestamp = "2015-06-29 14:39:05 +UTC" # get_prev_timestamp
+    			# current_timestamp = Time.new.getutc
+    			# return ((current_timestamp - previous_timestamp)/30.minutes).round > 0 ? true : false
+    			# # previous_timestamp ==> write method to get timestamp from db for particular device
     		return true
     	end
 
     	def run_diagnostic	
         	@diagnostic.each{ |diag|
-        		diag.run(@options)
-            	# Can catch result (like wifi => 'ok', memory => 'not ok') here and pass on to device db ? 
-        	}
+        		diag.run(@options) 
+           }
+		end
 
+		def repair
+				# Should be either called from here or from within diagnostic ? 
 		end
 	end
 end
