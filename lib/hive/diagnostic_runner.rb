06fbed1a343cf1c202213ff4a8952b147beb99c2
asim <asimk21@gmail.com>
2015-06-30 12:39:01 +0100

    Moved should_run? to diagnostic.rb

--- lib/hive/diagnostic_runner.rb
+++ lib/hive/diagnostic_runner.rb
@@ -1,34 +1,24 @@
 module Hive
-	class Diagnostic_Runner
+	class DiagnosticRunner
 		
 		def initialize(options, diagnostic_type)
-			@diagnostic = []
+			@diagnostics = []
 			@options = options
 			@diagnostic_type = diagnostic_type
 			@diagnostic_array = Hive.config.diagnostic[@diagnostic_type].keys 
-        	initialize_diagnostic(Hive.config.diagnostic) 
-        	run_diagnostic unless !should_run? 	
-    	end
+       end
 
     	def initialize_diagnostic(diagnostic_details)
       		@diagnostic_array.each { |component|
       			Hive.logger.info("Initializing #{component.capitalize} component for #{@diagnostic_type.capitalize} diagnostic")
         		require "hive/diagnostic/#{@diagnostic_type}/#{component}"
-        		@diagnostic << Object.const_get('Hive').const_get('Diagnostic').const_get(@diagnostic_type.capitalize).const_get(component.capitalize).new(diagnostic_details[@diagnostic_type][component])
+        		@diagnostics << Object.const_get('Hive').const_get('Diagnostic').const_get(@diagnostic_type.capitalize).const_get(component.capitalize).new(diagnostic_details[@diagnostic_type][component])
         	}
     	end 
 
-    	def should_run?
-    		 # 	previous_timestamp = "2015-06-29 14:39:05 +UTC" # get_prev_timestamp
-    			# current_timestamp = Time.new.getutc
-    			# return ((current_timestamp - previous_timestamp)/30.minutes).round > 0 ? true : false
-    			# # previous_timestamp ==> write method to get timestamp from db for particular device
-    		return true
-    	end
-
-    	def run_diagnostic	
-        	@diagnostic.each{ |diag|
-        		diag.run(@options) 
+    	def run	
+        	@diagnostics.each{ |diagnostic|
+        		diagnostic.run(@options) unless !diagnostic.should_run?
            }
 		end
 
