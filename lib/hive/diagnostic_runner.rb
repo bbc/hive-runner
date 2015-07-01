849219859023652ff901ee7b6a4d4bac2fa19b2d
David Buckhurst <david.buckhurst@bbc.co.uk>
2015-07-01 09:38:25 +0100

    Simplify run loop

--- lib/hive/diagnostic_runner.rb
+++ lib/hive/diagnostic_runner.rb
@@ -1,28 +1,26 @@
 module Hive
-	class DiagnosticRunner		
-		def initialize(options, diagnostics_type)
-			@diagnostics = []
-			@options = options
-			@diagnostics_type = diagnostics_type
-			@diagnostic_array = Hive.config.diagnostics[@diagnostics_type].keys 
+	class DiagnosticRunner
+	  attr_accessor :diagnostics
+	  
+		def initialize(options, diagnostics_config)
+			@diagnostics = self.initalize_diagnostics(diagnostics_config)
 		end
 
-		def initialize_diagnostic(diagnostics_details)
-			@diagnostic_array.each { |component|
+		def initialize_diagnostic(diagnostics_config)
+			diagnostic_config.collect do |component, config|
 				Hive.logger.info("Initializing #{component.capitalize} component for #{@diagnostics_type.capitalize} diagnostic")
 				require "hive/diagnostic/#{@diagnostics_type}/#{component}"
-				@diagnostics << Object.const_get('Hive').const_get('Diagnostic').const_get(@diagnostics_type.capitalize).const_get(component.capitalize).new(diagnostics_details[@diagnostics_type][component])
-			}
+				Object.const_get('Hive').const_get('Diagnostic').const_get(@diagnostics_type.capitalize).const_get(component.capitalize).new(diagnostics_details[@diagnostics_type][component])
+			end
 		end 
 
 		def run	
-			@diagnostics.each{ |diagnostic|
-				diagnostic.run(@options) unless !diagnostic.should_run?
-			}
+			results = diagnostics.collect do |diagnostic|
+				diagnostic.run
+		  end
+		  failures = results.select { |r| r.failed? }
+		  failures.count == 0
 		end
 
-		def repair
-			# Should be either called from here or from within diagnostic ? 
-		end
 	end
 end
