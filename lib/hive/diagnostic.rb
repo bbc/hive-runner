5f1ec0d29d5e85f88db57b05def71231742f0511
asim <asimk21@gmail.com>
2015-09-07 16:58:58 +0100

    Indentation Fix

--- lib/hive/diagnostic.rb
+++ lib/hive/diagnostic.rb
@@ -1,52 +1,52 @@
 require 'hive'
 require 'device_api/android'
 require 'hive/results'
-			
+      
 module Hive
-	class Diagnostic
-		
-		class InvalidParameterError < StandardError
-    	end
+  class Diagnostic
+    
+    class InvalidParameterError < StandardError
+      end
 
-		attr_accessor :config, :last_run, :device_api
+    attr_accessor :config, :last_run, :device_api
 
-		def initialize(config, options)
-			@options = options
-			@config = config
-			@serial = @options['serial']
-			@device_api = @options['device_api']
-		end
+    def initialize(config, options)
+      @options = options
+      @config = config
+      @serial = @options['serial']
+      @device_api = @options['device_api']
+    end
 
-		def should_run?
-			return true if @last_run == nil
-			time_now = Time.new.getutc
-			last_run_time = @last_run.timestamp
-			diff = ((time_now - last_run_time)/5.minutes).round
-			if (diff > 2 && @last_run.passed?) || diff > 1
-				true
-			else
-				false
-			end
-		end
+    def should_run?
+      return true if @last_run == nil
+      time_now = Time.new.getutc
+      last_run_time = @last_run.timestamp
+      diff = ((time_now - last_run_time)/5.minutes).round
+      if (diff > 2 && @last_run.passed?) || diff > 1
+        true
+      else
+        false
+      end
+    end
 
-		def run
-			Hive.logger.info("Trying to run diagnostic '#{self.class}'")
-			if should_run?	
-				result = diagnose 
-				result = repair(result) if result.failed?
-				@last_run = result
-			end
-			@last_run	
-		end
+    def run
+      Hive.logger.info("Trying to run diagnostic '#{self.class}'")
+      if should_run?  
+        result = diagnose 
+        result = repair(result) if result.failed?
+        @last_run = result
+      end
+      @last_run 
+    end
 
-		def pass(message= {}, data = {})
-			Hive.logger.info(message)
-			Hive::Results.new("pass", message, data )
-		end
+    def pass(message= {}, data = {})
+      Hive.logger.info(message)
+      Hive::Results.new("pass", message, data )
+    end
 
-		def fail(message ={}, data = {})
-			Hive.logger.info(message)
-			Hive::Results.new("fail", message, data)
-		end
-	end
+    def fail(message ={}, data = {})
+      Hive.logger.info(message)
+      Hive::Results.new("fail", message, data)
+    end
+  end
 end
--- lib/hive/diagnostic_runner.rb
+++ lib/hive/diagnostic_runner.rb
@@ -1,28 +1,28 @@
 module Hive
-	class DiagnosticRunner
-		attr_accessor :diagnostics, :options
+  class DiagnosticRunner
+    attr_accessor :diagnostics, :options
 
-		def initialize(options, diagnostics_config, platform)
-			@options = options
-			@platform = platform
-			@diagnostics = self.initialize_diagnostics(diagnostics_config[@platform])
-		end
+    def initialize(options, diagnostics_config, platform)
+      @options = options
+      @platform = platform
+      @diagnostics = self.initialize_diagnostics(diagnostics_config[@platform])
+    end
 
-		def initialize_diagnostics(diagnostics_config)
-			@diagnostics = diagnostics_config.collect do |component, config|
-				Hive.logger.info("Initializing #{component.capitalize} component for #{@platform.capitalize} diagnostic")
-				require "hive/diagnostic/#{@platform}/#{component}"
-				Object.const_get('Hive').const_get('Diagnostic').const_get(@platform.capitalize).const_get(component.capitalize).new(config, @options)
-			end
-		end 
+    def initialize_diagnostics(diagnostics_config)
+      @diagnostics = diagnostics_config.collect do |component, config|
+        Hive.logger.info("Initializing #{component.capitalize} component for #{@platform.capitalize} diagnostic")
+        require "hive/diagnostic/#{@platform}/#{component}"
+        Object.const_get('Hive').const_get('Diagnostic').const_get(@platform.capitalize).const_get(component.capitalize).new(config, @options)
+      end
+    end 
 
-		def run
-			results = @diagnostics.collect do |diagnostic|
-				diagnostic.run
-			end
-			
-			failures = results.select { |r| r.failed? }
-			failures.count == 0
-		end
-	end
+    def run
+      results = @diagnostics.collect do |diagnostic|
+        diagnostic.run
+      end
+      
+      failures = results.select { |r| r.failed? }
+      failures.count == 0
+    end
+  end
 end
