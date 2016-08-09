d5f56898212de0eca94f81179aa129c378144a7e
asim <asimk21@gmail.com>
2016-08-09 12:00:00 +0100

    Code_cache fixed and hence reverted comment

--- lib/hive/worker.rb
+++ lib/hive/worker.rb
@@ -149,12 +149,12 @@ module Hive
         @log.info "Setting job paths"
         @file_system = Hive::FileSystem.new(@job.job_id, Hive.config.logging.home, @log)
         set_job_state_to :preparing
-        #if ! @job.repository.to_s.empty?
-        #  @log.info "Checking out the repository"
-        #  @log.debug "  #{@job.repository}"
-        #  @log.debug "  #{@file_system.testbed_path}"
-        #  checkout_code(@job.repository, @file_system.testbed_path)
-        #end
+        if ! @job.repository.to_s.empty?
+          @log.info "Checking out the repository"
+          @log.debug "  #{@job.repository}"
+          @log.debug "  #{@file_system.testbed_path}"
+          checkout_code(@job.repository, @file_system.testbed_path)
+        end
 
         @log.info "Initialising execution script"
         @script = Hive::ExecutionScript.new(
@@ -361,7 +361,6 @@ module Hive
     end
     
     def testmine_config(checkout)
-      Dir.glob( "#{checkout}/.testmi{n,t}e.yml" ).first
     end
 
     def lion_config(checkout)
