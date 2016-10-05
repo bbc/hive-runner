1947f00c45a249ce3d538201f333e8e3bb44c9fd
jonpwilson <jon.wilson01@bbc.co.uk>
2016-10-05 11:02:52 +0100

    Added test for tar.gz file

--- spec/hive/file_system_spec.rb
+++ spec/hive/file_system_spec.rb
@@ -7,14 +7,19 @@ RSpec.describe Hive::FileSystem do
     before(:all) do
       `touch test.sh`
       `zip test.zip test.sh`
+      `tar -czvf test.tar.gz test.sh`
     end
 
     after(:all) do
-      `rm test.zip test.sh`
+      `rm test.zip test.sh tar.tar.gz`
     end
 
     it 'checks zip file integrity correctly' do
       expect(Hive::FileSystem.new(0, Dir.mktmpdir, Hive::Log.new).check_build_integrity('test.zip')).to be true
+    end
+
+    it 'checks tar.gz file integrity correctly' do
+      expect(Hive::FileSystem.new(0, Dir.mktmpdir, Hive::Log.new).check_build_integrity('test.tar.gz')).to be true
     end
 
   end
